3a7b37c50ada5c9d25d59ca9f79098ba3a10dbe6
i-3 <i-3@i3>
2018-08-07 11:29:50 +0530

    First project

---
+++ Looping/src/info/inetsolv/forloop/App.java
@@ -0,0 +1,22 @@
+package info.inetsolv.forloop;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int i=3;
+		/*for(;++i<=9;){
+			System.out.println("hello---"+i);
+		}*/
+		
+		/*for(;;){
+			System.out.println("infinate");
+		}*/
+		
+		
+		for(System.out.println("hai");i<=7;System.out.println("bye")){
+			System.out.println("hello");
+		}
+	}
+
+}
---
+++ Looping/src/info/inetsolv/forloop/WhileLoop.java
@@ -0,0 +1,15 @@
+package info.inetsolv.forloop;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+
+		
+		int n=8;int i=1;
+		do{
+			System.out.println(n+"*"+i+"="+(n*i));
+			i++;
+		}while(i<=10);
+	}
+
+}
